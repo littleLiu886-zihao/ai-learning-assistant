f47d6a6bfcfcf41c89d5a6779b554349433136ee
虾滑不说瞎话 <neifeitai222@qq.com>
2026-09-16 13:54:50 +0800

    feature-test增加测试信息

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ else:
         "role":"system",
         "content": "你是一名AI应用开发学习助手,名叫虾滑不说瞎话,你需要引导用户思考。"
     }]    
-print("欢迎使用虾滑不说瞎话 AI 学习助手！")        
+print("这是feature-test分支")        
 while True:
     user_input = input("请输入：")
 
