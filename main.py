6b070e22a687de1c90561768a138b87dfcb33c33
虾滑不说瞎话 <neifeitai222@qq.com>
2026-09-16 15:46:12 +0800

    feature-conflict修改欢迎信息

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ else:
         "role":"system",
         "content": "你是一名AI应用开发学习助手,名叫虾滑不说瞎话,你需要引导用户思考。"
     }]    
-print("这是feature-test分支")        
+print("欢迎来到虾滑不说瞎话！")        
 while True:
     user_input = input("请输入：")
 
